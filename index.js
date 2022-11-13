4db99d61334c627bb2a4fa59dd785d1e453737ba
LeftClickMage <leftclickmage@gmail.com>
2022-11-12 21:04:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ const io = new Server(server);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/ubuntu-v20-latin-500.eot', (req, res) => {
+  res.sendFile(__dirname + '/ubuntu-v20-latin-500.eot');
+});
+app.get('/ubuntu-v20-latin-500.eot?#iefix', (req, res) => {
+  res.sendFile(__dirname + '/ubuntu-v20-latin-500.eot?#iefix');
+});
+app.get('/ubuntu-v20-latin-500.woff2', (req, res) => {
+  res.sendFile(__dirname + '/ubuntu-v20-latin-500.woff2');
+});
+app.get('/ubuntu-v20-latin-500.woff', (req, res) => {
+  res.sendFile(__dirname + '/ubuntu-v20-latin-500.woff');
+});
+
+app.get('/ubuntu-v20-latin-500.ttf', (req, res) => {
+  res.sendFile(__dirname + '/ubuntu-v20-latin-500.ttf');
+});
+app.get('/ubuntu-v20-latin-500.svg#Ubuntu', (req, res) => {
+  res.sendFile(__dirname + '/ubuntu-v20-latin-500.svg#Ubuntu');
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -26,13 +45,13 @@ io.on('connection', (socket) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('move', (msg, val, val2) => {
-    io.emit('move', msg, val, val2);
+  socket.on('move', (msg, val, val2, valR, atk) => {
+    io.emit('move', msg, val, val2, valR, atk);
   });
 });
 
 io.on('connection', (socket) => {
-  socket.on('update', (msg) => {
-    io.emit('update', msg);
+  socket.on('update', (user, msg) => {
+    io.emit('update', user, msg);
   });
 });
