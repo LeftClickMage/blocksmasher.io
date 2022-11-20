745efaa086d73d18fb0177be29abde3239efe587
LeftClickMage <leftclickmage@gmail.com>
2022-11-20 09:49:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(2096, () => {
   console.log('listening on *:3000');
 });
 io.on('connection', (socket) => {
