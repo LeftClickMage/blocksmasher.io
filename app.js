b96cf77a278bad7b9981dd1640deea4d99cd5d12
LeftClickMage <leftclickmage@gmail.com>
2022-11-22 20:34:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
 io.on('connection', (socket) => {
   socket.on('spawn player', (msg) => {
     io.emit('spawn player', msg);
