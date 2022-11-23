03c9fbab31624e0f2344bd1675046b1fd75d363b
LeftClickMage <leftclickmage@gmail.com>
2022-11-22 20:46:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,57 +18,57 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname,'..', '/')));
+// app.use(express.static(path.join(__dirname,'..', '/')));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/arena.html');
 });
-app.get('/ubuntu-v20-latin-500.eot', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.eot');
-});
-app.get('/ubuntu-v20-latin-500.eot?#iefix', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.eot?#iefix');
-});
-app.get('/ubuntu-v20-latin-500.woff2', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.woff2');
-});
-app.get('/ubuntu-v20-latin-500.woff', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.woff');
-});
-
-app.get('/ubuntu-v20-latin-500.ttf', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.ttf');
-});
-app.get('/ubuntu-v20-latin-500.svg#Ubuntu', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.svg#Ubuntu');
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log("disconnected");
-  });
-});
-
-// server.listen(3000, () => {
-//   console.log('listening on *:3000');
+// app.get('/ubuntu-v20-latin-500.eot', (req, res) => {
+//   res.sendFile(__dirname + '../ubuntu-v20-latin-500.eot');
 // });
-io.on('connection', (socket) => {
-  socket.on('spawn player', (msg) => {
-    io.emit('spawn player', msg);
-  });
-});
+// app.get('/ubuntu-v20-latin-500.eot?#iefix', (req, res) => {
+//   res.sendFile(__dirname + '../ubuntu-v20-latin-500.eot?#iefix');
+// });
+// app.get('/ubuntu-v20-latin-500.woff2', (req, res) => {
+//   res.sendFile(__dirname + '../ubuntu-v20-latin-500.woff2');
+// });
+// app.get('/ubuntu-v20-latin-500.woff', (req, res) => {
+//   res.sendFile(__dirname + '../ubuntu-v20-latin-500.woff');
+// });
 
-io.on('connection', (socket) => {
-  socket.on('move', (msg, val, val2, valR, atk) => {
-    io.emit('move', msg, val, val2, valR, atk);
-  });
-});
+// app.get('/ubuntu-v20-latin-500.ttf', (req, res) => {
+//   res.sendFile(__dirname + '../ubuntu-v20-latin-500.ttf');
+// });
+// app.get('/ubuntu-v20-latin-500.svg#Ubuntu', (req, res) => {
+//   res.sendFile(__dirname + '../ubuntu-v20-latin-500.svg#Ubuntu');
+// });
 
-io.on('connection', (socket) => {
-  socket.on('update', (user, msg) => {
-    io.emit('update', user, msg);
-  });
-});
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log("disconnected");
+//   });
+// });
+
+// // server.listen(3000, () => {
+// //   console.log('listening on *:3000');
+// // });
+// io.on('connection', (socket) => {
+//   socket.on('spawn player', (msg) => {
+//     io.emit('spawn player', msg);
+//   });
+// });
+
+// io.on('connection', (socket) => {
+//   socket.on('move', (msg, val, val2, valR, atk) => {
+//     io.emit('move', msg, val, val2, valR, atk);
+//   });
+// });
+
+// io.on('connection', (socket) => {
+//   socket.on('update', (user, msg) => {
+//     io.emit('update', user, msg);
+//   });
+// });
 
 
 app.set('views', path.join(__dirname, 'views'));
