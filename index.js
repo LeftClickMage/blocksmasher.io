e4358c2b230339197f8328c140e51070b2ea80f3
fairopshotgun <leftclickmage@gmail.com>
2022-11-27 09:53:56 -0500

    attackUpdate

--- index.js
+++ index.js
@@ -7,29 +7,12 @@ const path = require('path');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-// app.use(express.static(path.join(__dirname,'..', '/')));
+app.use(express.static(path.join(__dirname,'..', '/')));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/arena.html');
 });
-app.get('/ubuntu-v20-latin-500.eot', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.eot');
-});
-app.get('/ubuntu-v20-latin-500.eot?#iefix', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.eot?#iefix');
-});
-app.get('/ubuntu-v20-latin-500.woff2', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.woff2');
-});
-app.get('/ubuntu-v20-latin-500.woff', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.woff');
-});
 
-app.get('/ubuntu-v20-latin-500.ttf', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.ttf');
-});
-app.get('/ubuntu-v20-latin-500.svg#Ubuntu', (req, res) => {
-  res.sendFile(__dirname + '../ubuntu-v20-latin-500.svg#Ubuntu');
-});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
